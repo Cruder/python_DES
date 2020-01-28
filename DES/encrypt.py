a1d259be6ea85a524a61ed5b91aae98c90464aab
Alexandre Lairan <lairana@free.fr>
2020-01-28 09:46:39 +0100

    Refacto

--- DES/encrypt.py
+++ DES/encrypt.py
@@ -1,5 +1,3 @@
-import sys
-from DES.key import Key
 from DES.constants import Constants
 from DES.extractor import Extractor
 from DES.packager import Packager
@@ -9,11 +7,47 @@ from DES.round import Round
 from DES.ConvAlphaBin import conv_bin, nib_vnoc
 
 
+class RoundsEncrypt:
+    def __init__(self, sub_keys, constants):
+        self.sub_keys = sub_keys
+        self.constants = constants
+
+    def __call__(self, packet):
+        for i in range(16):
+            round = Round(self.sub_keys[i], self.constants.perm(), self.constants.expansion(), self.constants.substitutions())
+            left = packet[0:32]
+            right = packet[32:]
+            left, right = round(left, right)
+            packet = left + right
+        return packet
+
+
+class RoundsDecrypt:
+    def __init__(self, sub_keys, constants):
+        self.sub_keys = sub_keys
+        self.constants = constants
+
+    def __call__(self, packet):
+        for i in range(15, -1, -1):
+            round = Round(self.sub_keys[i], self.constants.perm(), self.constants.expansion(), self.constants.substitutions())
+            left = packet[0:32]
+            right = packet[32:]
+            left, right = round(right, left)
+            packet = right + left
+        return packet
+
+
+modes = {
+    "encrypt": RoundsEncrypt,
+    "decrypt": RoundsDecrypt
+}
+
+
 class Encrypt:
     def __init__(self):
         pass
 
-    def __call__(self, message, key):
+    def __call__(self, message, key, mode):
         # Convert message to binary
         message = conv_bin(message)
 
@@ -34,65 +68,17 @@ class Encrypt:
         # sub_keys : Array(16)
         sub_keys = key.extract_sub_keys(extractor)
 
-        print(sub_keys)
-
         # packets : Array(64)
         packets = packager(message)
         new_packets = []
 
-        for packet in packets:
-            # packet : String | Permuted using PI
-            packet = initial_permutator(packet, constants.pi())
-            # packet = '0111110110101011001111010010101001111111101100100000001111110010'
-
-            for i in range(16):
-                round = Round(sub_keys[i], constants.perm(), constants.expansion(), constants.substitutions())
-                left = packet[0:32]
-                right = packet[32:]
-                left, right = round(left, right)
-                packet = left + right
-
-            packet = initial_permutator(packet, constants.inv_pi())
-            new_packets.append(packet)
-        return ''.join(new_packets)
-
-
-class Decrypt:
-    def __init__(self):
-        pass
-
-    def __call__(self, message, key):
-        # Fetch constants from file
-        constants = Constants()
-
-        # Slice the message into a 64bit data
-        packager = Packager(64)
-
-        # Permute data for messages
-        initial_permutator = Permutator(offset=-1)
-
-        # Class to extract 16 sub key from the key
-        # Initialized from CP1 and CP2
-        extractor = Extractor(constants.cp1(), constants.cp2())
-
-        # Extract sub keys
-        # sub_keys : Array(16)
-        sub_keys = key.extract_sub_keys(extractor)
-
-        # packets : Array(64)
-        packets = packager(message)
-        new_packets = []
+        rounds = modes[mode](sub_keys, constants)
 
         for packet in packets:
             # packet : String | Permuted using PI
             packet = initial_permutator(packet, constants.pi())
 
-            for i in range(15, -1, -1):
-                round = Round(sub_keys[i], constants.perm(), constants.expansion(), constants.substitutions())
-                left = packet[0:32]
-                right = packet[32:]
-                left, right = round(right, left)
-                packet = right + left
+            packet = rounds(packet)
 
             packet = initial_permutator(packet, constants.inv_pi())
             new_packets.append(packet)
--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import sys
 from DES.key import Key
-from DES.encrypt import Encrypt, Decrypt
-
-from DES.ConvAlphaBin import conv_bin, nib_vnoc
+from DES.encrypt import Encrypt
 
 if __name__ == '__main__':
-    message = "Hello World my friend"
+    message = "Hello World my friend :)"
 
     key_filename = sys.argv[1]
     key = Key.from_file(key_filename)
 
-    key = Key("0101111001011011010100100111111101010001000110101011110010010001")
+    # key = Key("0101111001011011010100100111111101010001000110101011110010010001")
 
     encrypt = Encrypt()
-    result = encrypt(message, key)
+    result = encrypt(message, key, "encrypt")
 
     print('result', result)
 
-    # decrypt = Decrypt()
-    # result2 = decrypt(result, key)
-    #
-    # print('result 2 ', result2)
-    #
-    #
-    # print(nib_vnoc("1101110010111011110001001101010111100110111101111100001000110010100111010010101101101011111000110011101011011111"))
-    # print(conv_bin("!LvE.eb!wjKdK,vjOtè"))
+    # result = ''
+    # f = open(sys.argv[2])
+    # line = f.readline()
+    # while line != '':
+    #     result += line
+    #     line = f.readline()
+
+    decrypt = Encrypt()
+    result2 = decrypt(result, key, "decrypt")
+
+    print('result 2 ', result2)
